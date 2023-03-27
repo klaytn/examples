0d3d1bcf52f108a1a0771f800629c1382e02fc8e
ayo-klaytn <peter.ayo@krustuniverse.com>
2023-03-27 13:30:15 +0100

    updated

--- sdk-&-libraries-for-interacting-with-klaytn-node/web3-js/read.js
+++ sdk-&-libraries-for-interacting-with-klaytn-node/web3-js/read.js
@@ -1,20 +1,21 @@
 const Web3 = require('web3');
 
-const url = 'https://klaytn-baobab-rpc.allthatnode.com:8551/qtKkeUE8ZEPI2cs0OHloJ6seI4Wfy36N' 
+const url = "RPC URL"  
 const web3 = new Web3(url);
+
+const privateKey = "Paste private key";
 
 async function getLatestBlock() {
     const latestBlock = await web3.eth.getBlockNumber();
     console.log(latestBlock.toString());
 }
 
-
-
 async function getKlayBalance() {
-    const klayBalance  = await web3.eth.getBalance("0x8B93b7d7Ed0960e9A0090851334e19b3c451E4E9")
+    const klayBalance  = await web3.eth.getBalance("Paste wallet address")
     const formatBalance = await web3.utils.fromWei(klayBalance)
     console.log(`You have ${formatBalance} KLAY`)  
 }
 
+// call the following functions
 getLatestBlock();
 getKlayBalance();
