3a599db8ec01ebed14c2296a764fb479fe1a2e4d
khoa-klaytn <mk.tran@klaytn.foundation>
2024-04-04 15:42:12 +0800

    tools/graphprotocol/erc20-subgraph: add docs for mapping.ts

--- tools/graphprotocol/erc20-subgraph/src/mapping.ts
+++ tools/graphprotocol/erc20-subgraph/src/mapping.ts
@@ -2,12 +2,17 @@ import { BigInt, log } from "@graphprotocol/graph-ts";
 import { Transfer } from "./types/MySubgraph/MyToken";
 import { User } from "./types/schema";
 
+/**
+ * @name handleTransfer matches the eventHandler defined in subgraph.yaml
+ */
 export function handleTransfer(event: Transfer): void {
   let user = User.load(event.params.from.toHex());
   if (user) {
+    // +1 to the number of transfers initiated by `from`
     user.n_transfers = user.n_transfers.plus(BigInt.fromI32(1));
   } else {
     user = new User(event.params.from.toHex());
+    // or start from 1 if it's `from`'s first transfer
     user.n_transfers = BigInt.fromI32(1);
   }
 
