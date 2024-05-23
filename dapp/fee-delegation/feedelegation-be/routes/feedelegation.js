37863c1ffde7440da0d6bdcd6a920d5372a955f3
Paul Elisha <ireajao@yahoo.com>
2024-05-23 09:40:18 +0100

    refactored code and handled error properly

--- dapp/fee-delegation/feedelegation-be/routes/feedelegation.js
+++ dapp/fee-delegation/feedelegation-be/routes/feedelegation.js
@@ -4,21 +4,25 @@ const Caver = require('caver-js')
 const RPC_URL = process.env.RPC_URL || "https://api.baobab.klaytn.net:8651";
 
 router.post('/', async (req, res) => {
+  const caver = new Caver(new Caver.providers.HttpProvider(RPC_URL, {}));
+  let feePayerPrivateKey = process.env.FEE_PAYER_PRIVATE_KEY || "";
+  let feePayerAddress = process.env.FEE_PAYER_PUBLIC_ADDRESS || "";
+
   try {
+    if (req.body == "" && req.method != "POST") return;
+
     const { deployTx } = req.body;
 
-    const caver = new Caver(new Caver.providers.HttpProvider(RPC_URL, {}));
-    if(!process.env.FEE_PAYER_PRIVATE_KEY) {
-      throw new Error("Please configure fee payer private key");
-    }
     // Feepayer keyring generation
-    let feePayerPrivateKey = process.env.FEE_PAYER_PRIVATE_KEY;
-    let feePayerAddress = process.env.FEE_PAYER_PUBLIC_ADDRESS; //caver.klay.accounts.privateKeyToPublicKey(feePayerPrivateKey);
+    if (!feePayerPrivateKey) throw new Error("configure fee payer private key");
+
+    if (!feePayerAddress) throw new Error("fee payer address: null");
 
     const feePayerKeyring = caver.wallet.keyring.create(feePayerAddress, feePayerPrivateKey)
     caver.wallet.add(feePayerKeyring)
 
-    let deployTxDecoded = caver.transaction.decode(deployTx);
+    const { deployTxDecoded } = caver.transaction.decode(deployTx);
+    if (deployTxDecoded.type != "TxTypeSmartContractDeploy") return
 
     // Signs the transaction as a fee payer
     await caver.wallet.signAsFeePayer(feePayerKeyring.address, deployTxDecoded);
@@ -26,10 +30,10 @@ router.post('/', async (req, res) => {
     // Transaction execution
     const receipt = await caver.rpc.klay.sendRawTransaction(deployTxDecoded);
 
-    return res.status(200).json({success: true, contractAddress: receipt.contractAddress });
+    return res.status(200).json({ success: true, contractAddress: receipt.contractAddress });
 
-  } catch(err) {
-    res.status(500).json({success: false, message: err.message});
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
   }
 });
 
